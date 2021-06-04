8a084b3a7d39a2bbdf235ec96589fc019fefb01f
Kesara Rathnayake <kesara@fq.nz>
2021-06-04 13:40:21 +1200

    Disable tarball upload

--- scripts/copy-snapshot.sh
+++ scripts/copy-snapshot.sh
@@ -11,7 +11,8 @@ tar cjfv --ignore-failed-read ietf_utf8.bin.tar.bz2 mysql
 
 ls -la ietf_utf8.bin.tar.bz2
 
-echo "Copying tarball to upstream server"
-scp ietf_utf8.bin.tar.bz2 $UPSTREAMSERVER:$UPSTREAMPATH
+#echo "Copying tarball to upstream server"
+#scp ietf_utf8.bin.tar.bz2 $UPSTREAMSERVER:$UPSTREAMPATH
+echo "This tarball will be copied to $UPSTREAMSERVER:$UPSTREAMPATH in the future."
 
 echo "Done!"
