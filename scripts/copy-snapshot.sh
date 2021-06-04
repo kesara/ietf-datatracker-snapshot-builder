ca8410e14915fce096f1933a12c0115cea62241c
Kesara Rathnayake <kesara@fq.nz>
2021-06-04 12:26:37 +1200

    Use custom script to build tarball

--- scripts/copy-snapshot.sh
+++ scripts/copy-snapshot.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-cd ~/$CODEDIR
-
-# copy db
 echo "Running copydb"
-docker/copydb -v
+
+cd ~/$CODEDIR/data/
+
+echo "Working direcory: $PWD"
+
+echo "Building tarball"
+tar cjfv --ignore-failed-read ietf_utf8.bin.tar.bz2 mysql
+
+ls -la ietf_utf8.bin.tar.bz2
+
+echo "Copying tarball to upstream server"
+scp ietf_utf8.bin.tar.bz2 $UPSTREAMSERVER:$UPSTREAMPATH
 
 echo "Done!"
